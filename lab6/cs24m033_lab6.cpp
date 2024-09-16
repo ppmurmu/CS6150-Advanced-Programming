25d1438e34d5edb10eb6eba5f906f1f7db240499
ppmurmu <pradeeppetermurmu@gmail.com>
2024-09-16 20:22:27 +0530

    lab6 fixed

--- lab6/cs24m033_lab6.cpp
+++ lab6/cs24m033_lab6.cpp
@@ -2,233 +2,280 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
 /* No other header file should be used */
 
-
-
-class LIFO {
-    public:
+class LIFO
+{
+public:
     int db[3000];
     int db_size;
     int cache[10001];
     int top;
     LIFO(int size)
     {
-        db_size=size;
-        top=-1;
-        for(int i=0; i< 10001; i++)
+        db_size = size;
+        top = -1;
+        for (int i = 0; i < 10001; i++)
         {
-            cache[i]=-1;
+            cache[i] = -1;
         }
     }
     void put(int key, int value)
     {
-        if(top==db_size-1)
+        if (cache[key] != -1)
         {
-            cache[db[top]]=-1;
-            db[top]=key;
-            cache[key]=value;
+            cache[key] = value;
+            return;
+        }
+        if (top == db_size - 1)
+        {
+            cache[db[top]] = -1;
+            db[top] = key;
+            cache[key] = value;
 
             return;
         }
         top++;
-        db[top]=key;
-        cache[key]=value;
-
+        db[top] = key;
+        cache[key] = value;
     }
     int get(int k)
     {
-        if(cache[k]==-1)
+        if (cache[k] == -1)
         {
             return -1;
         }
-        
+
         return cache[k];
     }
     void print()
     {
-        if(top>-1)
+        if (top > -1)
         {
-             for(int i=top; i >=0; i--)
+            for (int i = top; i >= 0; i--)
             {
                 cout << db[i] << " ";
-            }            
+            }
         }
         cout << endl;
-       
     }
-    
-    
 };
 
 class Node
 {
-    public:
+public:
     int val;
     int key;
     Node *left;
     Node *right;
     Node(int k, int v)
     {
-        val=v;
-        key=k;
-        left=NULL;
-        right=NULL;
+        val = v;
+        key = k;
+        left = NULL;
+        right = NULL;
     }
 };
 
 class LRU
 {
-    public:
+public:
     Node *dbPointer[10001];
-    int db_size;
     int cache[10001];
     Node *front;
     Node *rear;
     int capacity;
     int curr;
+
     LRU(int n)
     {
-        capacity=n;
-        curr=0;
-        for(int i=0; i< 10001; i++)
+        capacity = n;
+        curr = 0;
+        front = NULL;
+        rear = NULL;
+        for (int i = 0; i < 10001; i++)
         {
-            cache[i]=-1;
+            cache[i] = -1;
+            dbPointer[i] = NULL;
         }
-        front=NULL;
-        rear=NULL;
     }
+
     int get(int k)
     {
-        if(cache[k]==-1)
+        if (cache[k] == -1 || dbPointer[k] == NULL)
         {
             return -1;
         }
-        Node *t=dbPointer[k];
-        //when we get, we evict and then add it to the back
-        put(k, t->val);
-        return cache[k];
+
+        Node *t = dbPointer[k];
+        int tempVal = t->val;
+
+        // Remove node from its current position
+        if (t->left)
+            t->left->right = t->right;
+        else
+            front = t->right;
+
+        if (t->right)
+            t->right->left = t->left;
+        else
+            rear = t->left;
+
+        // Move to rear
+        if (rear)
+        {
+            rear->right = t;
+            t->left = rear;
+            t->right = NULL;
+            rear = t;
+        }
+        else
+        {
+            front = rear = t;
+            t->left = t->right = NULL;
+        }
+
+        return tempVal;
     }
+
     void put(int key, int val)
     {
-        if(curr==capacity)
+        if (dbPointer[key] != NULL)
         {
-            front=front->right;
-            cache[key]=-1;
-            Node *newNode=new Node(key, val);
-            newNode->left=rear;
-            rear=newNode;
-            dbPointer[key]=newNode;            
+            // Key exists, update value and move to rear
+            dbPointer[key]->val = val;
+            get(key); // This will move the node to rear
             return;
         }
-        if(curr==0)
+
+        Node *newNode = new Node(key, val);
+
+        if (curr == capacity)
         {
-            Node *newNode=new Node(key, val);
-            cache[key]=val;
-            front=newNode;
-            rear=newNode;
-            curr++;
-            return;
+            // Remove from front
+            Node *temp = front;
+            front = front->right;
+            if (front)
+                front->left = NULL;
+            else
+                rear = NULL;
+
+            cache[temp->key] = -1;
+            dbPointer[temp->key] = NULL;
+            delete temp;
+            curr--;
         }
-        Node *newNode= new Node(key, val);
-        cache[key]=val;
-        newNode->left=rear;
-        rear=newNode;
-        dbPointer[key]=newNode;
+
+        // Add new node to rear
+        if (rear)
+        {
+            rear->right = newNode;
+            newNode->left = rear;
+            rear = newNode;
+        }
+        else
+        {
+            front = rear = newNode;
+        }
+
+        cache[key] = val;
+        dbPointer[key] = newNode;
         curr++;
     }
-    
+
     void print()
     {
-        Node *t=rear;
-        while(t!=NULL)
+        Node *t = rear;
+        while (t != NULL)
         {
             cout << t->key << " ";
-            t=t->left;
+            t = t->left;
         }
         cout << endl;
     }
 };
 
-
-
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    
-    string inp="";
-    LIFO lifo=LIFO(0);
-    LRU lru= LRU(0);
-    string cat="";
-    while(inp != "done")
-    {  
-        
-        cin>> inp;
-        if(inp=="LIFO")
-        {
-            cat="LIFO";
-            int size;
-            cin >> size;
-            lifo=LIFO(size);
-        }
-        if(inp=="put" && cat=="LIFO")
-            {
-                int k ,val;
-                cin >> k >> val;
-                lifo.put(k,val);
-            }
-        if(inp=="get" && cat=="LIFO")
-                {
-                    int val;
-                    cin >> val;
-                    cout << lifo.get(val) << endl;
-            }
-            if(inp == "done")
-            {
-                break;
-            }
-            
-                if(inp=="print" && cat=="LIFO")
-                {
-                    lifo.print();
-            }
-            if(inp == "LRU")
-            {
-                cat="LRU";
-                int size;
-                cin >> size;
-                lru=LRU(size);
-            }
-       
-        if(inp=="put" && cat=="LRU")
-            {
-                int k ,val;
-                cin >> k >> val;
-                lru.put(k,val);
-            }
-        if(inp=="get" && cat=="LRU")
-                {
-                    int val;
-                    cin >> val;
-                    cout << lru.get(val) << endl;
-            
-            }
-            if(inp == "done")
-            {
-                break;
-            }
-            
-                if(inp=="print" && cat=="LRU")
-                {
-                    lru.print();
-            }
-        
-        
+int main()
+{
+    freopen("my_output.txt", "w", stdout);
+    ifstream inputFile("inp10.txt");
+    if (!inputFile.is_open())
+    {
+        cerr << "Error opening file" << endl;
+        return 1;
     }
-    
-        
-        
-    
+    string inp = "";
+    LIFO lifo = LIFO(0);
+    LRU lru = LRU(0);
+    string cat = "";
+    cout << inp << endl;
+    while (inp != "done")
+    {
+
+        inputFile >> inp;
+        if (inp == "LIFO")
+        {
+            cat = "LIFO";
+            int size;
+            inputFile >> size;
+            lifo = LIFO(size);
+        }
+        if (inp == "put" && cat == "LIFO")
+        {
+            int k, val;
+            inputFile >> k >> val;
+            lifo.put(k, val);
+        }
+        if (inp == "get" && cat == "LIFO")
+        {
+            int val;
+            inputFile >> val;
+            cout << lifo.get(val) << endl;
+        }
+        if (inp == "done")
+        {
+            break;
+        }
+
+        if (inp == "print" && cat == "LIFO")
+        {
+            lifo.print();
+        }
+        if (inp == "LRU")
+        {
+            cat = "LRU";
+            int size;
+            inputFile >> size;
+            lru = LRU(size);
+        }
+
+        if (inp == "put" && cat == "LRU")
+        {
+            int k, val;
+            inputFile >> k >> val;
+            lru.put(k, val);
+        }
+        if (inp == "get" && cat == "LRU")
+        {
+            int val;
+            inputFile >> val;
+            int temp = lru.get(val);
+            cout << temp << endl;
+        }
+        if (inp == "done")
+        {
+            break;
+        }
+
+        if (inp == "print" && cat == "LRU")
+        {
+            lru.print();
+        }
+    }
+
     return 0;
 }
