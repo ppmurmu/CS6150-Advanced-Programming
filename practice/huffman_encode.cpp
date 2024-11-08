6bf2c8ee3741cf282718bf050ff114a04d4a7b8b
ppmurmu <pradeeppetermurmu@gmail.com>
2024-11-08 23:53:23 +0530

    edit 1

---
+++ practice/huffman_encode.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    vector<pair<char, int>> freq={{'A', 4}, {'B', 3}, {'C', 8}, {'D', 7}};
+    priority_queue<pair<c
+    
+
+}
--- practice/permuation.cpp
+++ practice/permuation.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-bool checkExists(vector<int> &curr, int n)
-{
-    auto it = find(curr.begin(), curr.end(), n);
-    return (it != curr.end());
-}
+// bool checkExists(vector<int> &curr, int n)
+// {
+//     auto it = find(curr.begin(), curr.end(), n);
+//     return (it != curr.end());
+// }
 
-vector<vector<int>> recursion(vector<int> &a, vector<vector<int>> curr)
-{
-    if (curr.size() == a.size())
-    {
-        return curr;
-    }
+// vector<vector<int>> recursion(vector<int> &a, vector<vector<int>> curr)
+// {
+//     if (curr.size() == a.size())
+//     {
+//         return curr;
+//     }
 
-    for (int i = 0; i < a.size(); i++)
-    {
-        // ddoesnt exxist in curr
-        if (checkExists(curr, a[i]) == false)
-        {
-            curr.push_back(a[i]);
-            recursion(a, curr);
-            curr.pop_back();
-        }
-    }
-}
+//     for (int i = 0; i < a.size(); i++)
+//     {
+//         // ddoesnt exxist in curr
+//         if (checkExists(curr, a[i]) == false)
+//         {
+//             curr.push_back(a[i]);
+//             recursion(a, curr);
+//             curr.pop_back();
+//         }
+//     }
+// }
 
-int main()
-{
-    vector<int> a = {1, 2, 3};
+// int main()
+// {
+//     vector<int> a = {1, 2, 3};
 
-    vector<int> res = recursion(a, {});
+//     vector<int> res = recursion(a, {});
 
-    for (int i = 0; i < res.size(); i++)
-    {
-        cout <<
-    }
-}
+//     for (int i = 0; i < res.size(); i++)
+//     {
+//         cout <<
+//     }
+// }
